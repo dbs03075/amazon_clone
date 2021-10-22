f5c60881156c6a2a3b31bbcdd50d73005d776c34
dbs03075@gmail.com <70051860+dbs03075@users.noreply.github.com>
2021-10-22 21:55:38 +0900

    10-22

---
+++ backend/data.js
@@ -0,0 +1,96 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shiet',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand:'Nike',
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shiet',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand:'Adidas',
+            countInStock: 10,
+            rating: 4.0,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'3',
+            name:'Nike Slim Shiet',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price: 120,
+            brand:'Nike',
+            countInStock: 10,
+
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'4',
+            name:'Lacoste Slim Shiet',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price: 120,
+            brand:'Lacoste',
+            countInStock: 10,
+
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'5',
+            name:'Nike Slim Shiet',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price: 120,
+            brand:'Nike',
+            countInStock: 10,
+
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'6',
+            name:'Nike Slim Shiet',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price: 120,
+            brand:'Nike',
+            countInStock: 10,
+
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:'7',
+            name:'Nike Slim Shiet',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price: 120,
+            brand:'Nike',
+            countInStock: 0,
+
+            rating: 3,
+            numReviews:10,
+            description:'high quality product',
+        },
+
+    ],
+};
+
+export default data;
